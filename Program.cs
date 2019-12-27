17d80e8e9a846c10d6a7b2add74f3ff420f832fa
d-kiselev <dkiselev1234@gmail.com>
2019-12-27 18:23:23 +0300

    add script invocation

--- Program.cs
+++ Program.cs
@@ -11,8 +11,6 @@ namespace wavesbridgetokentransfer
     {
         public static void Main(string[] args)
         {
-            SetTokenPortScript(); return;
-
             // transfer tokens from stagenet to testnet
 
             var testnetNode = new Node(Node.TestNetChainId);
@@ -27,10 +25,14 @@ namespace wavesbridgetokentransfer
             var Bob = "3MuR872m3WiW1DRBD8CfoLbZpJgo3xzLyy7"; // in testnet
 
             // 1. transfer tokens from Alice to TokenPort (in stagenet)
-            var response = stagenetNode.Transfer(Alice, tokenPortStagenet, Assets.WAVES, 0.00000002m, 0.005m, null, Bob.FromBase58()); // Bob.FromBase58() ???
-            Thread.Sleep(3000);
+            /*var response = stagenetNode.Transfer(Alice, tokenPortStagenet, Assets.WAVES, 0.00000002m, 0.005m, null, Bob.FromBase58()); // Bob.FromBase58() ???
+            Thread.Sleep(10000);
 
             var txId = response.ParseJsonObject().GetString("id");
+            Console.WriteLine($"transfer tx id: {txId}");
+            */
+            var txId = "3bsuQrwX2QM9g2WqXBEYNs1RUHrSPJzGeJo1Jpj17o2p";
+
             var blockHeight = stagenetNode.GetTransactionHeight(txId);
             var txBytes = stagenetNode.GetTransactionById(txId).GetBytes();
             var key = blockHeight.ToString() + "_merkleRoot";
@@ -44,27 +46,39 @@ namespace wavesbridgetokentransfer
             }
 
             // 3. genegare MerkleProof (for transaction in stagenet)
-            var merkleProof = new byte[32]; // ???
+            var merkleProof = GenerateMerkleProof(stagenetNode, txId, blockHeight);
 
             // 4. invoke script of tokenPort (in testnet) --> Bob receives money
-            var caller = PrivateKeyAccount.CreateFromSeed("seed", testnetNode.ChainId);
+            var callerSeed = "whip disagree egg satisfy repeat engine envelope federal toward shoulder cattle rare much lava melt";
+            var caller = PrivateKeyAccount.CreateFromSeed(callerSeed, testnetNode.ChainId);
             testnetNode.InvokeScript(caller,tokenPortTestnet,"withdraw",new List<object> { txBytes, (long)blockHeight, merkleProof });
         }
 
-        public static void SetTokenPortScript()
+        public static byte[] GenerateMerkleProof(Node stagenetNode, string txId, int blockHeight)
+        {
+            return new byte[32];
+        }
+
+        public static void SetTokenPort()
         {
             var node = new Node(Node.TestNetChainId);
             var chainId = 'T';
             var tokenPortSeed = "seed take purity craft away cake month layer napkin nasty void entire theme slam explain";
             var tokenPort = PrivateKeyAccount.CreateFromSeed(tokenPortSeed, chainId);
 
+            node.PutData(tokenPort, new Dictionary<string, object>
+            {
+                { "tokenPortInOtherChain", "3Mgqt4HLefP3bafouR4gXPrP9EgU4pajTct".FromBase58() },
+                { "chainCollector", "3N6VvqXLuZ4uxWG6E92orif1KR61bqF6JCk".FromBase58()}
+            },0.01m);
+
+            var tokenPortScript = "";
             var assembly = Assembly.GetExecutingAssembly();
-            var stream = assembly.GetManifestResourceStream("waves-bridge-token-transfer.Resources.TokenPort.ride");
-            var reader = new StreamReader(stream);
-            var tokenPortScript = reader.ReadToEnd();
-            
-            Console.WriteLine(tokenPortScript);
-            return;
+            using (var stream = assembly.GetManifestResourceStream("wavesbridgetokentransfer.Resources.TokenPort.ride"))
+            using (var reader = new StreamReader(stream))
+            {
+                tokenPortScript = reader.ReadToEnd();
+            }
 
             var compiledScript = node.CompileCode(tokenPortScript);
             node.SetScript(tokenPort, compiledScript);
