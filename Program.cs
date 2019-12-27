f8d05f06c0b563874fa12c00df48752101a79cd2
d-kiselev <dkiselev1234@gmail.com>
2019-12-27 14:55:16 +0300

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wavesbridgetokentransfer
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
